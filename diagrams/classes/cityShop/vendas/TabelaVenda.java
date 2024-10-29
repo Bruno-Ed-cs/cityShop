a8486d4cead79a9f9f66dae1360c38528cd0e13b
Bruno-Ed-cs <brunosilva.pinf@gmail.com>
2024-10-29 15:10:46 -0300

    singleton tabela venda

--- diagrams/classes/cityShop/vendas/TabelaVenda.java
+++ diagrams/classes/cityShop/vendas/TabelaVenda.java
@@ -4,4 +4,32 @@ public class TabelaVenda {
 	
 	public Integer qtdVenda;
 	public ArrayList<Venda> vendas;
+	private TabelaVenda instancia;
+
+	private TabelaVenda() {
+
+
+	}
+
+	public TabelaVenda getInstance() {
+
+		if (this.instancia == null) {
+
+
+			this.instancia = new TabelaVenda();
+
+		}
+
+		return this.instancia;
+
+	}
+
+	public Integer getQtdVenda() {
+
+	}
+
+	public ArrayList<Venda> getVendas() {
+		
+	}
+
 }
