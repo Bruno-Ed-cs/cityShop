98f33b20c69d383c2543d4e952d7ecee081070b8
kelryana <ana20240012783@alu.uern.br>
2024-10-29 14:52:38 -0300

    Update FavoritoProduto.java

--- diagrams/classes/cityShop/estoque/FavoritoProduto.java
+++ diagrams/classes/cityShop/estoque/FavoritoProduto.java
@@ -3,10 +3,34 @@ package cityShop.estoque;
 import cityShop.app.*;
 import cityShop.usuario.*;
 
+//ajustar a classe "FavoritoProduto" pra que possa ser adicionado aos favoritos do usuário
+
 public class FavoritoProduto implements Favoritavel
 {
 
 	public Produto target;
 	public Usuario publisher;
 	
+
+	//criando um construtor pra iniciar
+
+	public FavoritoProduto(Produto target, Usuario publiher) {
+
+		this.target = target;
+		this.publisher = publisher;
+	}
+
+	//implementar os métodos na interface (fazer o construtor, implementar os métodos...) 
+
+	@Override
+	public void adicionarAosFavoritos(Usuario usuario) {
+
+		usuario.adicionarProdutoFavorito(this);
+	}
+
+	@Override
+	public void removerDosFavoritos(Usuario usuario) {
+
+		usuario.getProdutosFavoritos().remove(this);
+	}
 }
