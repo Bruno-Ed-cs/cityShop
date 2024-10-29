66000fe469bc3341cb6343ab85b27ccf1e525096
kelryana <ana20240012783@alu.uern.br>
2024-10-29 15:07:07 -0300

    Update Avaliacao.java

--- diagrams/classes/cityShop/app/Avaliacao.java
+++ diagrams/classes/cityShop/app/Avaliacao.java
@@ -7,5 +7,12 @@ public class Avaliacao {
 	public Integer nota;
 	public String corpo;
 	public Usuario autor;
+
+	public Avaliacao(Integer nota, String corpo, Usuario autor) {
+
+		this.nota = nota;
+		this.corpo = corpo;
+		this.autor = autor;
+	}
 	
 }
