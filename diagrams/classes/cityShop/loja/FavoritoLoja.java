1a25940ba049a076ce3134b8e2fba2187ddc8c67
kelryana <ana20240012783@alu.uern.br>
2024-10-29 15:03:24 -0300

    Update FavoritoLoja.java

--- diagrams/classes/cityShop/loja/FavoritoLoja.java
+++ diagrams/classes/cityShop/loja/FavoritoLoja.java
@@ -8,4 +8,22 @@ public class FavoritoLoja implements Favoritavel
 	public Loja target;
 	public Usuario publisher;
 	
+
+	public FavoritoLoja(Loja target, Usuario publisher) {
+
+		this.target = target;
+		this.publisher = publisher;
+	}
+
+	@Override
+	public void adicionarAosFavoritos(Usuario usuario) {
+
+		usuario.adicionarFavoritoLoja(this);
+	}
+
+	@Override 
+	public void removerDosFavoritos(Usuario usuario) {
+
+		usuario.getProdutosFavoritos().remove(this);
+	}
 }
