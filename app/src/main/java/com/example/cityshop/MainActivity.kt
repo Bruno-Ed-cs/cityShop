8764871a81b0c3ecfb41ba326484854140d3d86d
Bruno-Ed-cs <brunosilva.pinf@gmail.com>
2024-11-07 20:05:49 -0300

    android studio

--- app/src/main/java/com/example/cityshop/MainActivity.kt
+++ app/src/main/java/com/example/cityshop/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cityshop.ui.theme.CityShopTheme
+import org.cityShop.app.*;
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             CityShopTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val cat: Categoria = Categoria()
+                    cat.nome = "cell"
                     Greeting(
-                        name = "Android",
+
+                        name = cat.nome,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
