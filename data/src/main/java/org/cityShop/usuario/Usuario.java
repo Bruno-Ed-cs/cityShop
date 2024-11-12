196182c9a0753d4a62cadcda87c56640965f9f74
kelryana <ana20240012783@alu.uern.br>
2024-11-12 14:41:27 -0300

    correção do pacote usuario

--- data/src/main/java/org/cityShop/usuario/Usuario.java
+++ data/src/main/java/org/cityShop/usuario/Usuario.java
@@ -1,10 +1,11 @@
 package org.cityShop.usuario;
 
 import java.util.ArrayList;
-import org.cityShop.produto.*;
+import org.cityShop.estoque.*;
 import org.cityShop.loja.*;
 
 public class Usuario 
+
 {
 
 	public String nome;
@@ -14,52 +15,5 @@ public class Usuario
 	public ArrayList<FavoritoLoja> lojasFavoritas;
 	public ArrayList<FavoritoProduto> produtosFavoritos;
 
-	//implementação singleton, com a instancia unica
-	private static Usuario instancia;
 
-	//aplicação do construtor privadp
-	private Usuario(String nome, String cpf, Long id) {
-
-		this.nome = nome;
-		this.cpf = cpf;
-		this.id = id;
-
-		this.lojasFavoritas = new ArrayList<>();
-		this.produtosFavoritos = new ArrayList<>();
-	}
-	
-	//criação do método que vai acessar a instancia unica
-
-	public static Usuario getInstance(String nome, String cpf, Long id) {
-
-		if(instancia == null) {
-
-			instancia = new Usuario(nome, cpf, id);
-		}
-
-		return instancia;
-	} 
-
-
-	//agora, vai estar aplicando os métodos para a adição dos favoritos
-
-	public void adicionarFavoritoProduto(FavoritoProduto favoritoProduto) {
-
-		produtosFavoritos.add(favoritoProduto);
-	}
-
-	public void adicionarFavoritoLoja(FavoritoLoja favoritoLoja) {
-
-		lojasFavoritas.add(favoritoLoja);
-	}
-
-	public ArrayList<FavoritoProduto> getProdutosFavoritos() {
-
-		return produtosFavoritos;
-	} 
-
-	public ArrayList<FavoritoLoja> getLojasFavoritas() {
-
-		return lojasFavoritas;
-	}
 }
