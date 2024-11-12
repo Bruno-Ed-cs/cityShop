7be357e09d6ebaeff9ed2f5fb78f68158eb24630
kelryana <ana20240012783@alu.uern.br>
2024-11-12 14:12:08 -0300

    FavoritoLoja

--- data/src/main/java/org/cityShop/usuario/FavoritoLoja.java
+++ data/src/main/java/org/cityShop/usuario/FavoritoLoja.java
@@ -3,11 +3,14 @@ package org.cityShop.loja;
 import org.cityShop.app.*;
 import org.cityShop.usuario.Usuario;
 
+//classe "FavoritoLoja" adiciona as lojas favoritas do usuário
+
 public class FavoritoLoja implements Favoritavel
 {
 
-	public Loja target;
-	public Usuario publisher;
+
+	public Loja target; //Loja favorita
+	public Usuario publisher; //Usuário que adicionou a loja aos favoritos
 	
 
 	public FavoritoLoja(Loja target, Usuario publisher) {
