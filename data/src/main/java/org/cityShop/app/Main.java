dfab3b8a62cabcd2c78e76a5e2b7fa8ad044d4dd
Ana Kelry <anakelry346@gmail.com>
2024-11-27 20:44:33 -0300

    Update Main.java

--- data/src/main/java/org/cityShop/app/Main.java
+++ data/src/main/java/org/cityShop/app/Main.java
@@ -1,23 +1,28 @@
 package org.cityShop.app;
 
-import java.sql.Date;
-
 public class Main {
 	
 	public static void main(String[] args) {
 
+		// Obter as instâncias únicas de App e Database
+
 		App app = App.getInstance();
 		Database database = Database.getInstance();
 
+		// Chamada de teste para o 
+		
 		database.teste();
 
+		// Instanciar a interface com o usuário (Tui)
+
 		Tui tui = new Tui();
-		tui.mainMenu();
+
+		// Limpar o terminal antes de exibir o menu
 
 		Tui.clearTerminal();
 
-			
-
-
+		// Exibir o menu para o usuário interagir
+		
+		tui.mainMenu();
 	}
 }
